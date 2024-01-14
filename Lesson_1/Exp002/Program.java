73faa54fd5f797dfb6129cbd1a0754dfc62719fc
Mayya <majko2594@gmail.com>
2024-01-14 21:28:16 +0300

    update exp002

--- Lesson_1/Exp002/Program.java
+++ Lesson_1/Exp002/Program.java
@@ -10,14 +10,13 @@ public class Program {
     }
 
     public static void main(String[] args) {
-        Point2D a = new Point2D();
-        a.x = 0;
-        a.y = 2;
-        System.out.println(a.toString());
+        Point2D a = new Point2D(0, 2);
 
-        Point2D b = new Point2D();
-        b.x = 0;
-        b.y = 10;
+        System.out.println(a);
+        //System.out.println(a.getInfo()); - вывести точку с помощью метода getInfo, если бы он был public
+
+        Point2D b = new Point2D(10);
+
         System.out.println(b.toString());
         System.out.println(distance(a, b));
     }
