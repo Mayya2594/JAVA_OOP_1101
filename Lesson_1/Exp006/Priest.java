6f1a5de2658164f6333cb874b7f232131083c431
Mayya <majko2594@gmail.com>
2024-01-15 17:05:40 +0300

    add exp006

---
+++ Lesson_1/Exp006/Priest.java
@@ -0,0 +1,21 @@
+package Lesson_1.Exp006;
+
+//import java.util.Random;
+
+public class Priest extends BaseHero {
+
+    private int elixir;
+    private int maxElixir;
+
+    public Priest() {
+        super(String.format("Hero_Priest #%d", ++Priest.number),
+                Priest.r.nextInt(100, 200));
+        this.maxElixir = Priest.r.nextInt(50, 150);
+        this.elixir = maxElixir;
+    }
+
+    public String getInfo() {
+        return String.format("%s  Elixir: %d", super.getInfo(), this.elixir);
+    }
+
+}
